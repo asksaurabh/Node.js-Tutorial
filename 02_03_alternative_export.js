af9f02579129189224fb7526d7ffad5864a167fe
Saurabh Kumar <asksaurabh101@gmail.com>
2023-01-31 12:11:44 +0530

    Alternate syntax to export - Export as you go

---
+++ 02_03_alternative_export.js
@@ -0,0 +1,9 @@
+// Alternative way to export.
+// Export as you go.
+
+module.exports.userItems = ["BAT", "CAT"];
+module.exports.userInfo = {
+  userName: "John Doe",
+};
+
+// console.log(module);
--- 02_modules.js
+++ 02_modules.js
@@ -3,12 +3,18 @@
 
 const userNames = require("./02_01_names");
 const greetUser = require("./02_02_utils");
+const { userItems, userInfo } = require("./02_03_alternative_export");
 
 // console.log(userNames);
-
 greetUser(userNames.john);
 greetUser(userNames.peter);
 greetUser("Susan");
 
 // This won't work as secret variable is not exported.
 // greetUser(userNames.secret);
+
+userItems.forEach((item) => {
+  console.log(item);
+});
+
+console.log(userInfo.userName);
