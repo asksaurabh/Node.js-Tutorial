27a30a48c5f57c67dd913bcac32c96d4da8a3aa4
Saurabh Kumar <asksaurabh101@gmail.com>
2023-02-02 14:47:15 +0530

    Request object properties - url, method

---
+++ 02_Express_Tutorial/codes/03_request_object_methods.js
@@ -0,0 +1,23 @@
+// Various properties of request object.
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  // console.log(req.method);
+  const url = req.url;
+
+  if (url === "/") {
+    res.writeHead(200, { "content-type": "text/html" });
+    res.write("<h1>Home Page</h1>");
+    res.end();
+  } else if (url === "/about") {
+    res.writeHead(200, { "content-type": "text/html" });
+    res.write("<h1>This is the About Page</h1>");
+    res.end();
+  } else {
+    res.writeHead(404, { "content-type": "text/html" });
+    res.write("<h1>Error Not Found</h1>");
+    res.end();
+  }
+});
+
+server.listen(5000);
