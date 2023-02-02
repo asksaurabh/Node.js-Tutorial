0e236bdff39af7b74e9d94625e7a71e5fafec3b0
Saurabh Kumar <asksaurabh101@gmail.com>
2023-02-02 16:04:22 +0530

    Request properties

--- 02_Express_Tutorial/codes/03_request_object_methods.js
+++ 02_Express_Tutorial/codes/03_request_object_methods.js
@@ -5,15 +5,20 @@ const server = http.createServer((req, res) => {
   // console.log(req.method);
   const url = req.url;
 
+  // Home Page
   if (url === "/") {
     res.writeHead(200, { "content-type": "text/html" });
     res.write("<h1>Home Page</h1>");
     res.end();
-  } else if (url === "/about") {
+  }
+  // About page
+  else if (url === "/about") {
     res.writeHead(200, { "content-type": "text/html" });
     res.write("<h1>This is the About Page</h1>");
     res.end();
-  } else {
+  }
+  // Unavailable Resource
+  else {
     res.writeHead(404, { "content-type": "text/html" });
     res.write("<h1>Error Not Found</h1>");
     res.end();
