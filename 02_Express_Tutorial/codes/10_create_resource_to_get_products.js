6ee5bebbeedebb8a6fd8483dcd6e74f9abeac2d4
Saurabh Kumar <asksaurabh101@gmail.com>
2023-02-03 17:12:19 +0530

    Create a resource to get selected details of all products

---
+++ 02_Express_Tutorial/codes/10_create_resource_to_get_products.js
@@ -0,0 +1,21 @@
+// Create a resource to get only id name and image of all products.
+
+const express = require("express");
+const { products } = require("./data.js");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send(`<h1>Home Page</h1><a href="/api/products">Products</a>`);
+});
+
+app.get("/api/products", (req, res) => {
+  const newProducts = products.map((product) => {
+    const { id, name, image } = product;
+    return { id, name, image };
+  });
+  res.send(newProducts);
+});
+
+app.listen(5000, () => {
+  console.log("Server is listening at port 5000...");
+});
